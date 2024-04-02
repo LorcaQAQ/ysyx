1da5b0a5f018392f113938334d0a47b62008c937
tracer-ysyx <tracer@ysyx.org>
2024-04-02 20:08:06 +0800

    > sim RTL ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:06 up  2:27,  4 users,  load average: 0.55, 0.38, 0.36

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <Vlight.h>
+#include <nvboard.h>
+#include "verilated.h"
+
+void single_cycle() {
+	  top->clk = 0; top->eval();
+		  top->clk = 1; top->eval();
+}
+
+void reset(int n) {
+	  top->rst = 1;
+		  while (n -- > 0) single_cycle();
+			  top->rst = 0;
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
+
+	VerilatedContext *contextp=new VerilatedContext;
+	contextp->commandArgs(argc,argv);
+	Vlight *light=new Vlight{contextp};
+
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+
+	reset(10);
+	while(1){
+		single_cycle();
+		nvboard_update():
+	}
+	nvboard_quit():
   return 0;
 }
