b6335ded19bf1d39f9a4f50dcbdbd20e6fdcae23
tracer-ysyx <tracer@ysyx.org>
2024-04-15 22:04:29 +0800

    > compile NEMU ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:04:29 up  2:26,  3 users,  load average: 0.24, 0.17, 0.18

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,6 +24,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+	int count=sizeof(regs)/sizeof(regs[0]);
+	for(int i=0;i<count;i++){
+	printf(" ");
+	}
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
