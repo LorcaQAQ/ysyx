f6d26e02b3daeac61b434945c45ca1f0d6fc2af0
tracer-ysyx <tracer@ysyx.org>
2024-04-15 18:02:26 +0800

    > sim RTL ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:02:26 up 15 min,  1 user,  load average: 0.20, 0.21, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include <Vlight.h>
+#include <nvboard.h>
+
+static TOP_NAME light;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+void single_cycle() {
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
+}
+
+void reset(int n) {
+  top->rst = 1;
+  while (n -- > 0) single_cycle();
+  top->rst = 0;
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+	nvboard_bind_all_pins(&light);
+  nvboard_init();
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
 }
