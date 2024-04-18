8f27385dfaffddd1478cce0fcb3bd8e0f0a75f44
tracer-ysyx <tracer@ysyx.org>
2024-04-18 13:52:19 +0800

    > compile NEMU ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:52:19 up 15 min,  3 users,  load average: 0.14, 0.22, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -39,6 +39,8 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
 	{"\\*", '*'},         // times
+	{"\\-", '-'},					// substraction
+	{"/",   '/'},	        // division 
   {"==", TK_EQ},        // equal
 };
 
