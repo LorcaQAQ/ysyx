9438aff1f7476e3ffe22db6848af0af890415130
tracer-ysyx <tracer@ysyx.org>
2024-04-28 02:40:29 +0800

    > compile NEMU ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:40:29 up  8:37,  4 users,  load average: 0.46, 0.43, 0.42

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -73,7 +73,7 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[66532] __attribute__((used)) = {};//change from 32 to 66532
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
