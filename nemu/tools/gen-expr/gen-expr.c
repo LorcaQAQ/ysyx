d476ae3ad7d01db10e885681cd95a7087057b365
tracer-ysyx <tracer@ysyx.org>
2024-05-08 21:59:31 +0800

    > compile NEMU ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:59:31 up 7 min,  1 user,  load average: 0.50, 0.41, 0.23

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -44,7 +44,7 @@ static void gen_rand_and_or_op();
 static void gen_rand_expr() {
 	int branch;
 		if(expr_len<=65532){
-			branch=choose(7);
+			branch=choose(5);
 		}else{
 			branch=0;
 		}
@@ -53,8 +53,8 @@ static void gen_rand_expr() {
 					case 1: expr_len+=3;gen('('); gen_rand_expr(); gen(')'); break;
 					case 2: expr_len+=2;gen_space();gen_rand_expr();break;
 					case 3: expr_len+=2; gen_rand_logic_not();gen_rand_expr();break;
-					case 4: expr_len += 3; gen_rand_expr(); gen_rand_logic_op(); gen_rand_expr(); break;
-					case 5:expr_len += 3; gen_rand_expr(); gen_rand_and_or_op(); gen_rand_expr(); break;
+					case 4: expr_len += 4; gen_rand_expr(); gen_rand_logic_op(); gen_rand_expr(); break;
+					case 5:expr_len += 4; gen_rand_expr(); gen_rand_and_or_op(); gen_rand_expr(); break;
 					default: expr_len+=3;gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
 			}
 }
@@ -127,30 +127,27 @@ static int choose(int n){
 	return randnum;
 }
 
-//static void gen_negative(){
-//	buf[buf_index]='-';
-//	buf_index++;
-//}
+/*static void gen_negative(){
+	buf[buf_index]='-';
+	buf_index++;
+}*/
 static void gen_rand_logic_op() {
 	switch (choose(6)) {
-	case 0:buf[buf_index] = '>'; buf[buf_index++] = '='; break;
-	case 1:buf[buf_index] = '<'; buf[buf_index++] = '='; break;
-	case 2:buf[buf_index] = '='; buf[buf_index++] = '='; break;
-	case 3:buf[buf_index] = '!'; buf[buf_index++] = '='; break;
-	case 4:buf[buf_index] = '>'; break;
-	default:buf[buf_index] = '<'; break;
+	case 0:buf[buf_index++] = '>'; buf[buf_index++] = '='; break;
+	case 1:buf[buf_index++] = '<'; buf[buf_index++] = '='; break;
+	case 2:buf[buf_index++] = '='; buf[buf_index++] = '='; break;
+	case 3:buf[buf_index++] = '!'; buf[buf_index++] = '='; break;
+	case 4:buf[buf_index++] = '>'; break;
+	default:buf[buf_index++] = '<'; break;
 	}
-	buf_index++;
 }
 static void gen_rand_and_or_op() {
 	switch (choose(2)) {
-	case 0:buf[buf_index] = '|';buf[buf_index++] = '|'; break;
-	default:buf[buf_index] = '&'; buf[buf_index++] = '&';break;
+	case 0:buf[buf_index++] = '|';buf[buf_index++] = '|'; break;
+	default:buf[buf_index++] = '&'; buf[buf_index++] = '&';break;
 	}
-	buf_index++;
 }
 static void gen_rand_logic_not() {
-	buf[buf_index] = '!';
-	buf_index++;
+	buf[buf_index++] = '!';
 }
 
