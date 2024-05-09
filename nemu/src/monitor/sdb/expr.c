061d0ca34ef5d4632623a3b8a16c40b3d56d8cf5
tracer-ysyx <tracer@ysyx.org>
2024-05-09 14:38:57 +0800

    > compile NEMU ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:38:57 up 16 min,  1 user,  load average: 0.40, 0.32, 0.23

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -339,8 +339,8 @@ static int eval(int p, int q) {
 		switch (tokens[op_position].type) {
 		case '+': return val1 + val2;
 		case '-': return val1 - val2;
-		case '*': return (int)val1 * val2;
-		case '/': return (int)val1 / val2;
+		case '*': return (int)(val1 * val2);
+		case '/': return (int)(val1 / val2);
 		case EQU: return val1 == val2;
 		case NEQ: return val1 != val2;
 		case LEQ: return val1 <= val2;
