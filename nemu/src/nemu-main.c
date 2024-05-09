e4b3dc353a0f2aea4762929b9f0b50326a68d339
tracer-ysyx <tracer@ysyx.org>
2024-05-09 16:01:00 +0800

    > compile NEMU ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:01:00 up  1:39,  3 users,  load average: 0.59, 0.49, 0.31

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 
 	/*	To check expression value */
 	FILE *fp;
-	fp=fopen("./src/logic_expr_no_arithmatic.txt","r");
+	fp=fopen("./src/logic_expr_no_arithmetic.txt","r");
 	assert(fp!=NULL);
 	for(int i=0;!feof(fp);i++){
 		word_t true_result;
