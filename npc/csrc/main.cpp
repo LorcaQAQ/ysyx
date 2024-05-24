d04b9f5799ba12a59469ce7d23dd41bf2175d88f
tracer-ysyx <tracer@ysyx.org>
2024-05-24 21:29:37 +0800

    > compile NEMU ysyx_22040000 陆俊杰 Linux lorca-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:29:37 up  1:45,  2 users,  load average: 0.45, 0.26, 0.20

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -5,9 +5,22 @@
 
 #include <verilated_vcd_c.h>
 
-static uint32_t inst[]={
-
+static const uint32_t inst[]={
+0b0000000001100000000000010010011,//addi $ra,$$0,0x03
+0b0000000001100001000000100010011,//addi $sp,$ra,0x03
+0b1101111101100010000000110010011,//addi $gp,$sp,0x6fb
 }
+
+static uint32_t *init_mem(int n){
+  unint32_t *inst_list=(unint32_t *)malloc(n * sizeof(uint32_t));
+  memcpy(inst_list,inst,sizeof(inst));
+  return inst_list;
+}
+static uint32_t pmem_read(paddr_t addr, int len) {
+  uint32_t ret = host_read(addr-0x80000000, len);
+  return ret;
+}
+
 static void single_cycle(Vysyx_23060303_cputop* top) {
   top.clk = 0; top.eval();
   top.clk = 1; top.eval();
