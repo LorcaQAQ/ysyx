c47733ff6ef6a722a36262d7e778da5a2ba522d8
23060303-Junjie Lu <junjie1016@126.com>
2024-10-12 10:34:35 +0800

    :Complete mtrace

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -26,6 +26,8 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+void display_mem_read(paddr_t addr);
+void display_mem_write(paddr_t addr, word_t data);
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
@@ -51,6 +53,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  IFDEF(CONFIG_MTRACE,display_mem_read(addr));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -58,7 +61,17 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  IFDEF(CONFIG_MTRACE,display_mem_write(addr,data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
+}
+
+
+void display_mem_read(paddr_t addr){
+  printf("memory read at " FMT_PADDR ", PC=" FMT_WORD"\n", addr, cpu.pc);
+}
+
+void display_mem_write(paddr_t addr, word_t data){
+  printf("Memory write at " FMT_PADDR ", PC=" FMT_WORD", DATA is " FMT_WORD"\n", addr, cpu.pc,data);
 }
