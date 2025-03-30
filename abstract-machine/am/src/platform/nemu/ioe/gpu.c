0d7bf6c5b8e299c4ff38182b359590fa44731361
23060303-Junjie Lu <junjie1016@126.com>
2025-03-31 02:20:44 +0800

    Fix a bug in vga

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,22 +1,45 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <stdio.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static int height = 0;
+static int width = 0;
+
 void __am_gpu_init() {
+  uint32_t vgactl=inl(VGACTL_ADDR);
+  height = vgactl & 0xffff;  // TODO: get the correct width
+  width = (vgactl >> 16)& 0xffff;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  // uint32_t vgactl=inl(VGACTL_ADDR);
+  // height=vgactl & 0xffff;
+  // width=(vgactl >> 16)& 0xffff;
+  int vmemsz=height*width*sizeof(uint32_t);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = width, .height = height,
+    .vmemsz = vmemsz
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int w=ctl->w, h=ctl->h, x=ctl->x, y=ctl->y;
+  int offset=0;
+  uint32_t *pixels=ctl->pixels;
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }else {
+    for(int j=0;j < h; j++){
+      for(int i=0;i < w; i++){
+        offset=width*( y + j ) + x + i;
+        outl(FB_ADDR+offset*sizeof(uint32_t),pixels[w*j+i]);
+      }
+    }
   }
 }
 
